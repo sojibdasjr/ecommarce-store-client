ef68e10587f3fd42117ea783bb1aef1a7c989e95
sojibdasjr <contact.sojibdas@gmail.com>
2023-04-16 19:42:59 +0600

    first commit

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import{Link} from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h4 className="text-center">All Right Reserved &copy; WebTrue</h4>
+      <div className="text-center mt-3">
+          <Link to="/about">About</Link> 
+          |
+          <Link to= "/contact">Contact</Link> 
+          |
+          <Link to= "/policy">Privecy Policy</Link> 
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Helmet } from "react-helmet";
+
+const Layout = ({ children , description, keywords, author, title}) => {
+  return (
+    <div>
+      <Helmet>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords}/>
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
+
+      <Header />
+      <main style={{ minHeight: "81vh" }}>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+Layout.defaultProps ={
+    title:'Ecommerce app - default Page',
+    description:'mern stack projcet 2023',
+    keywords:'mern, react, node, mongodb, 2023',
+    author:'WebTrue'
+}
+
+export default Layout;
+
+
+// hudao hudai hudai hudaihudai
